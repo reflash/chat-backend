7fa67f84903ec2e0fde8fd43ac82cc5f3bc4ded6
reflash <reflash-ne@yandex.ru>
2016-12-07 15:17:13 +0300

    Adding travis-ci configs

--- build.fsx
+++ build.fsx
@@ -1,11 +1,78 @@
 // include Fake lib
 #r @"packages\FAKE\tools\FakeLib.dll"
-open Fake
 
-// Default target
-Target "Default" (fun _ ->
-    trace "Hello World from FAKE"
+open Fake
+open Fake.AssemblyInfoFile
+open Fake.Testing.NUnit3
+
+RestorePackages()
+
+// Directories
+let buildDir  = @".\build\"
+let testDir   = @".\test\"
+let deployDir = @".\deploy\"
+let packagesDir = @".\packages"
+
+// version info
+let version = "0.2"  // or retrieve from CI server
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDirs [buildDir; testDir; deployDir]
 )
 
+Target "SetVersions" (fun _ ->
+(*    CreateCSharpAssemblyInfo "./src/app/chat-backend/Properties/AssemblyInfo.cs"
+        [Attribute.Title "Chat-Backend"
+         Attribute.Description "A backend project for my chat fun"
+         Attribute.Guid "A539B42C-CB9F-4a23-8E57-AF4E7CEE5BAA"
+         Attribute.Product "Chat-Backend"
+         Attribute.Version version
+         Attribute.FileVersion version] *)
+
+    CreateCSharpAssemblyInfo "./src/app/chat-backend/Properties/AssemblyInfo.cs"
+        [Attribute.Title "Chat-Backend"
+         Attribute.Description "A backend project for my chat fun"
+         Attribute.Guid "EE5621DB-B86B-44eb-987F-9C94BCC98441"
+         Attribute.Product "Chat"
+         Attribute.Version version
+         Attribute.FileVersion version]
+)
+
+Target "CompileApp" (fun _ ->
+    !! @"src\app\**\*.fsproj"
+      |> MSBuildRelease buildDir "Build"
+      |> Log "AppBuild-Output: "
+)
+
+Target "CompileTest" (fun _ ->
+    !! @"src\test\**\*.fsproj"
+      |> MSBuildDebug testDir "Build"
+      |> Log "TestBuild-Output: "
+)
+
+Target "NUnitTest" (fun _ ->
+    !! (testDir + @"\*_test.dll")
+      |> NUnit3 (fun p ->
+                 {p with
+                   ToolPath = "packages/NUnit.ConsoleRunner/tools/nunit3-console.exe";
+                   OutputDir  = testDir + "TestResults.xml" })
+)
+
+
+Target "Zip" (fun _ ->
+    !! (buildDir + "\**\*.*")
+        -- "*.zip"
+        |> Zip buildDir (deployDir + "chat-backend." + version + ".zip")
+)
+
+// Dependencies
+"Clean"
+  ==> "SetVersions"
+  ==> "CompileApp"
+  ==> "CompileTest"
+  ==> "NUnitTest"
+  ==> "Zip"
+
 // start build
-RunTargetOrDefault "Default"
+RunTargetOrDefault "Zip"
